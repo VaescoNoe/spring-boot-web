d947a66b1ce1cdb5a1fb26297f989eaf6e3857bf
Noe Vargas <vaesconoe@gmail.com>
2020-07-23 00:51:46 -0500

    Se agrega Path variable

--- src/main/java/com/bolsadeideas/springboot/web/app/controller/EjemploParamsController.java
+++ src/main/java/com/bolsadeideas/springboot/web/app/controller/EjemploParamsController.java
@@ -13,7 +13,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 public class EjemploParamsController {
 	
 	
-	@GetMapping("/")
+	@GetMapping({"/",""})
 	public String index() {
 		return "params/index";
 	}
